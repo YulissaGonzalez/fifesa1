bbf072119a12c93b8a5e23a19ce4c664c05c8cb2
YulissaGonzalez <julissagonzalezgo2002@gmail.com>
2023-10-04 20:17:58 -0600

    creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadosController;
+use App\Http\Controllers\VacacionesController;
+use App\Http\Controllers\FiniquitosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('empleados/create',[EmpleadosController::class, 'create']);
+Route::resource('empleados',EmpleadosController::class);
+
+Route::get('vacaciones/create',[VacacionesController::class, 'create']);
+Route::resource('vacaciones',VacacionesController::class);
+
+Route::get('finiquitos/create',[FiniquitosController::class, 'create']);
+Route::resource('finiquitos',FiniquitosController::class);
+
 
 Auth::routes();
 
